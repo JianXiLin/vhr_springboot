3222399ffb94c1ead6cb0500374b73567578e126
JianxiLin <546839618@qq.com>
2020-06-03 00:25:48 +0800

    初始化项目

---
+++ src/test/java/com/jianxilin/vhr_springboot/VhrSpringbootApplicationTests.java
@@ -0,0 +1,14 @@
+package com.jianxilin.vhr_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VhrSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("demo");
+    }
+
+}
